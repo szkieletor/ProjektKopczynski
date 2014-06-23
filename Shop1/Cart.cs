3713cc14e1a28a0aec97738157612d5884d3856f
quantic92 <mati9.7.1992@wp.pl>
2014-06-23 13:34:11 +0200

    mm

---
+++ Shop1/Cart.cs
@@ -0,0 +1,19 @@
+﻿using Shop1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop1
+{
+    public class Cart
+    {
+        List<ProductCount> products = new List<ProductCount>();
+    
+
+public List<ProductCount> Products
+{
+  get { return products; }
+  set { products = value; }
+}}
+}
